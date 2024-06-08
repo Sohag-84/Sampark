8e1cc1e51b96330ceed22a529e3db409f2db04e5
Sohag-84 <ihsohag84@gmail.com>
2024-06-08 16:38:23 +0600

    set app theme

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:sampark/config/colors.dart';
 
 var lightTheme = ThemeData();
-var darkTheme = ThemeData();
+var darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  useMaterial3: true,
+  colorScheme: const ColorScheme.dark(
+    primary: dPrimarColor,
+    onPrimary: dBackgroundColor,
+    surface: dBackgroundColor,
+    onSurface: donContainerColor,
+    primaryContainer: dContainerColor,
+    onPrimaryContainer: dOnBackgroundColor,
+  ),
+);

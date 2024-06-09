ec92f749d5ee10fb839875d51dcdbfcedbb732e1
Sohag-84 <ihsohag84@gmail.com>
2024-06-09 16:07:13 +0600

    designed chat screen

--- lib/pages/chat/page/chat_page.dart
+++ lib/pages/chat/page/chat_page.dart
@@ -36,8 +36,57 @@ class ChatPage extends StatelessWidget {
           ),
         ],
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 5,
+        ),
+        margin: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.primaryContainer,
+          borderRadius: BorderRadius.circular(100),
+        ),
+        child: Row(
+          children: [
+            SizedBox(
+              width: 30,
+              height: 30,
+              child: SvgPicture.asset(
+                AssetsImage.chatMicSvg,
+                width: 25,
+              ),
+            ),
+            const Gap(10),
+            const Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                    filled: false, hintText: "Type message...."),
+              ),
+            ),
+            const Gap(10),
+            SizedBox(
+              width: 30,
+              height: 30,
+              child: SvgPicture.asset(
+                AssetsImage.chatGallarySvg,
+                width: 25,
+              ),
+            ),
+            const Gap(10),
+            SizedBox(
+              width: 30,
+              height: 30,
+              child: SvgPicture.asset(
+                AssetsImage.chatSendSvg,
+                width: 25,
+              ),
+            ),
+          ],
+        ),
+      ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(10.0),
         child: ListView(
           children: const [
             ChatBubble(
@@ -53,6 +102,14 @@ class ChatPage extends StatelessWidget {
               time: "10:12 PM",
               status: "read",
               imageUrl: "",
+            ),
+            ChatBubble(
+              message: "Fine.What about you?",
+              isComming: false,
+              time: "10:12 PM",
+              status: "read",
+              imageUrl:
+                  "https://w7.pngwing.com/pngs/348/885/png-transparent-flutter-dev-illustration.png",
             ),
           ],
         ),
